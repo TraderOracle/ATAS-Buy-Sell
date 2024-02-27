7888df3fe4ee4d53b748ad2b76dc6efe227efbe5
Trader Oracle <157963314+TraderOracle@users.noreply.github.com>
2024-02-27 03:03:40 -0600

    new

---
+++ copy_buysell.bat
@@ -0,0 +1,3 @@
+xcopy /y C:\Users\mitch\source\repos\ATASBuySell\bin\Debug\net7.0\ATASBuySell.dll C:\Users\mitch\Documents\ATAS\Indicators
+xcopy /y C:\Users\mitch\source\repos\ATASBuySell\BuySell.cs C:\Users\mitch\Documents\GitHub\ATAS
+xcopy /y C:\Users\mitch\source\repos\ATASBuySell\bin\Debug\net7.0\ATASBuySell.dll C:\Users\mitch\Documents\GitHub\ATAS
